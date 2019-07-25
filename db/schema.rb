c2e43a6c3df0314f626e943ca08c9d28eab42d82
Xinyao Zhang <xyzhang0817@gmail.com>
2019-07-25 16:22:40 -0400

    Students index.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_180945) do
+ActiveRecord::Schema.define(version: 2019_07_25_193834) do
 
   create_table "courses", force: :cascade do |t|
     t.string "shortname"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
