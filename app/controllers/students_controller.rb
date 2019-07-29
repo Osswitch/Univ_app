13bdc49e2ca108db64d7cf914fb34d16cd6d8602
Xinyao Zhang <xyzhang0817@gmail.com>
2019-07-29 11:40:28 -0400

    Restrict actions.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,8 @@
 class StudentsController < ApplicationController
 
 	before_action :set_student, only: [:show, :edit, :update]
+	skip_before_action :require_user, only: [:new, :create]
+	before_action :require_same_student, only: [:edit, :update]
 
 	def index
 		@students = Student.all
@@ -46,6 +48,13 @@ class StudentsController < ApplicationController
 
 	def student_params
 		params.require(:student).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	def require_same_student
+		if current_user != @student
+			flash[:notice] = "You can only edit your own user"
+			redirect_to student_path(current_user)
+		end
 	end
 
 end
